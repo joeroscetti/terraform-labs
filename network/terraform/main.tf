21224dbf47ce7cbfe4519fa25aebedd48e997840
Joe Roscetti <roscetti55@gmail.com>
2024-10-07 12:55:09 -0500

    main.tf

--- network/terraform/main.tf
+++ network/terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "github_testing" {
 }
 
 module "vnet" {
-  source              = "../modules/network"
+  source              = "../../modules/network"
   vnet_name           = var.vnet_name
   address_space       = var.address_space
   location            = var.location
