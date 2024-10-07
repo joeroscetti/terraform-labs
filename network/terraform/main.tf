f14bad0ecbb97e3380431aa0e215294adda6472f
Joe Roscetti <roscetti55@gmail.com>
2024-10-07 14:10:16 -0500

    main.tf

--- network/terraform/main.tf
+++ network/terraform/main.tf
@@ -5,14 +5,7 @@ provider "azurerm" {
 }
 
 # Remote state configuration
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "Terraform-state"
-    storage_account_name  = "terraformstateroscetti"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
+
 
 resource "azurerm_resource_group" "github_testing" {
   name     = "github_testing"
