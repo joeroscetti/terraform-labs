f1f932ef76e0b2d939dcb246fac7ea7d284dc876
Joe Roscetti <roscetti55@gmail.com>
2024-10-08 17:41:11 -0500

    main.tf

--- workloads/keyvault/terraform/main.tf
+++ workloads/keyvault/terraform/main.tf
@@ -14,3 +14,12 @@ resource "azurerm_resource_group" "github_keyvault" {
   location = "East US"
 }
 
+module "keyvault" {
+  source              = "../../../modules/keyvault"
+  kv_name           = var.kv_name
+  location            = azurerm_resource_group.github_keyvault.location
+  resource_group_name = azurerm_resource_group.github_keyvault.name
+  tenant_id           = var.tenant_id
+  object_id           = var.object_id
+ 
+}
