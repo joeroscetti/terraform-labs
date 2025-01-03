785233f905398e2f61cd1debb71c49acec58c06a
Joe Roscetti <roscetti55@gmail.com>
2025-01-03 09:42:14 -0600

    readme

--- bash/bash.sh
+++ bash/bash.sh
@@ -7,7 +7,7 @@ set -e
 AZURE_CLIENT_ID="5db4da55-82ec-4920-af14-db3eb20fbb7a"
 AZURE_CLIENT_SECRET="1Jp8Q~xJ4JnKBYxBw1UK9.7Ang-w8sz5iZDYidsQ"
 AZURE_TENANT_ID="a2bd4167-a08c-4878-828e-d9ea769bc007"
-DATABRICKS_HOST="workspace_testing_001"
+DATABRICKS_HOST="https://adb-2749952475382083.3.azuredatabricks.net"
 
 # Log into Azure
 echo "Logging into Azure with Service Principal..."
